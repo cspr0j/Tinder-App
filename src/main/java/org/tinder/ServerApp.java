3d1c7839d441e43e278f8071753de4d846d3b047
calal.aliyev <calal.aliyev@cybernet.az>
2022-10-16 03:23:59 +0400

    --add some patterns

--- src/main/java/org/tinder/ServerApp.java
+++ src/main/java/org/tinder/ServerApp.java
@@ -4,14 +4,15 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.tinder.servlets.LoginServlet;
+import org.tinder.servlets.RegistrationServlet;
 
 public class ServerApp {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
-        LoginServlet loginServlet = new LoginServlet();
 
         ServletContextHandler contextHandler = new ServletContextHandler();
-        contextHandler.addServlet(new ServletHolder(loginServlet), "/login");
+        contextHandler.addServlet(new ServletHolder(new LoginServlet()), "/login");
+        contextHandler.addServlet(new ServletHolder(new RegistrationServlet()), "/register");
         server.setHandler(contextHandler);
 
         server.start();
