ec249bc60bf08807b7413508542270edc91c9194
calal.aliyev <calal.aliyev@cybernet.az>
2022-10-24 00:47:20 +0400

    --test

---
+++ src/main/java/org/tinder/servlets/MessageServlet.java
@@ -0,0 +1,43 @@
+package org.tinder.servlets;
+
+import org.tinder.entities.Message;
+import org.tinder.service.MessageService;
+import org.tinder.service.UserService;
+import org.tinder.utils.CookieUtil;
+import org.tinder.utils.Freemarker;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+public class MessageServlet extends HttpServlet {
+
+    private Long senderID;
+    HashMap<String, Object> data = new HashMap<>();
+    UserService userService = new UserService();
+    Freemarker freemarker = new Freemarker();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        senderID = CookieUtil.getValue(req);
+        Long targetId = Long.parseLong(req.getPathInfo().replace("/", ""));
+        MessageService messageService = new MessageService(senderID);
+
+        List<Message> messages = messageService.getAllItemsByTargetId(senderID, targetId);
+        System.out.println(messages);
+
+        data.put("sender",userService.get(senderID));
+        data.put("receiver",userService.get(targetId));
+        data.put("messages", messages);
+        freemarker.render("chat.ftl", data, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
