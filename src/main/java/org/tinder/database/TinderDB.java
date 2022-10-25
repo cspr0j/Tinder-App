542069641e59af2086391bc38db0e28924a04f99
calal.aliyev <calal.aliyev@cybernet.az>
2022-10-26 00:20:04 +0400

    --final changes

--- src/main/java/org/tinder/database/TinderDB.java
+++ src/main/java/org/tinder/database/TinderDB.java
@@ -8,10 +8,10 @@ import java.sql.SQLException;
 
 @NoArgsConstructor
 public class TinderDB {
-    private static final String DB_URL = "jdbc:postgresql://localhost:5432/Tinder";
+    private static final String DB_URL = "jdbc:postgresql://ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/dbd0on8jtum1fm";
 
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "admin";
+    private static final String USERNAME = "zhzkoceyzzvtsg";
+    private static final String PASSWORD = "111bcf2952d9e70db0cfa61932e90ec8d06f9d5829cbc57aba4d61b4cf46350b";
 
     private static Connection connection;
 
