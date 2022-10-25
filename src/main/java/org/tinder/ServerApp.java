6189acc5f1ac511784ede22f074386bb347606ca
calal.aliyev <calal.aliyev@cybernet.az>
2022-10-26 01:39:19 +0400

    --deploy

--- src/main/java/org/tinder/ServerApp.java
+++ src/main/java/org/tinder/ServerApp.java
@@ -32,6 +32,7 @@ public class ServerApp {
 
         ServletContextHandler contextHandler = new ServletContextHandler();
         contextHandler.addServlet(new ServletHolder(new LoginServlet()), "/login");
+        contextHandler.addServlet(new ServletHolder(new LoginServlet()), "/");
         contextHandler.addServlet(new ServletHolder(new RegistrationServlet()), "/register");
         contextHandler.addServlet(new ServletHolder(new LogoutServlet()), "/logout");
         contextHandler.addServlet(new ServletHolder(new UserServlet()), "/users");
--- src/main/java/org/tinder/utils/UserGenerator.java
+++ src/main/java/org/tinder/utils/UserGenerator.java
@@ -17,15 +17,15 @@ public class UserGenerator {
 
         urls.put(0, Arrays.asList(
                 "https://media.istockphoto.com/photos/portrait-of-young-happy-indian-business-man-executive-looking-at-picture-id1309489745?b=1&k=20&m=1309489745&s=170667a&w=0&h=Wo_7nESC_ePyEYfEsnOm-rP6ElkxbWqIB3Ga4W3nw8M=",
-                "https://i.pinimg.com/originals/72/e6/11/72e6112287fecbcfd2ead4a4fc83ec82.jpg",
+                "https://media.gannett-cdn.com/29906170001/29906170001_5858202637001_5858201755001-vs.jpg?pubId=29906170001&width=660&height=371&format=pjpg&auto=webp",
                 "https://upload.wikimedia.org/wikipedia/commons/2/25/Leonardo_DiCaprio_2014.jpg",
                 "https://static.wikia.nocookie.net/avatar/images/e/e5/Rami_Malek.png"
         ));
         urls.put(1, Arrays.asList(
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxhcCYW4QDWMOjOuUTxOd50KcJvK-rop9qE9zRltSbVS_bO-cfWA",
                 "https://pbs.twimg.com/profile_images/1258426992841461760/BofEq3C-_400x400.jpg",
-                "https://blog.konusarakogren.com/wp-content/uploads/2019/08/rihanna-fb-feat-1280x720.jpg",
-                "https://voguecdn.blob.core.windows.net/files/img/article_detail/19-11/20/bella-hadid-123-1574242626.jpg"
+                "https://images.wallpapersden.com/image/download/naomi-scott-actress-2022_bWZqZmWUmZqaraWkpJRoa2WtaGtl.jpg",
+                "https://images.wallpapersden.com/image/download/emmy-rossum-hd-actress-2022_bWloZWyUmZqaraWkpJRoa2WtaGtl.jpg"
         ));
     }
 
